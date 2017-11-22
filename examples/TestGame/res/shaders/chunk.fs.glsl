cb3bb59b2688d7a759d694e2c5fffb314f659822
ninthworld <theninthworldix@gmail.com>
2017-11-22 11:22:36 -0800

    Redone

--- examples/TestGame/res/shaders/chunk.fs.glsl
+++ examples/TestGame/res/shaders/chunk.fs.glsl
@@ -6,8 +6,8 @@ out vec4 out_fragColor;
 
 uniform sampler2D texture0;
 
-void main() {
-
+void main(){
+	
 	vec3 texColor = texture(texture0, vs_texCoord).rgb;
 
 	out_fragColor = vec4(texColor, 1.0);
--- examples/TestGame/res/shaders/chunk.vs.glsl
+++ examples/TestGame/res/shaders/chunk.vs.glsl
@@ -1,15 +1,15 @@
 #version 330
 
-layout(location = 0) in vec3 in_position;
-layout(location = 1) in vec3 in_normal;
-layout(location = 2) in vec2 in_texCoord;
+layout (location = 0) in vec3 in_position;
+layout (location = 1) in vec3 in_normal;
+layout (location = 2) in vec2 in_texCoord;
 
 out vec2 vs_texCoord;
 
 uniform mat4 viewProjMatrix;
 uniform mat4 modelMatrix;
 
-void main() {
+void main(){
 	gl_Position = viewProjMatrix * modelMatrix * vec4(in_position, 1.0);
 	vs_texCoord = in_texCoord;
 }
